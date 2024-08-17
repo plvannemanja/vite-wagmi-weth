c335c249130054dbd7622c3a19edd198436277af
plvannemanja <nemanjaplvanmeth@gmail.com>
2024-08-16 23:22:58 -0700

    feat: chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx'
 import { config } from './wagmi.ts'
 
@@ -16,9 +16,11 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+      </ChakraProvider>
     </WagmiProvider>
   </React.StrictMode>,
 )
