52007897629c21b5ee98e6777706786d45ca418d
ghost-jin <ghostjvs1225@gmail.com>
2024-08-18 08:42:28 +0900

    fix-bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,125 @@
-import { useAccount } from 'wagmi'
+import {useAccount} from 'wagmi'
 import WrapUnwrapETH from './components/WrapUnWrapETH'
-import { Button, Flex, Spinner } from '@chakra-ui/react';
-import { SwapAmountProvider } from './context/SwapAmountContext';
+import {Button, Flex, Spinner, Box, Heading, Text, VStack, useToast} from '@chakra-ui/react';
+import {SwapAmountProvider} from './context/SwapAmountContext';
+import {useEffect, useState} from "react";
 
 const App: React.FC = () => {
-  const { isConnected, isConnecting } = useAccount();
-  if(isConnecting)
-    return (
-      <Flex
-      gap="5"
-      my="auto"
-      p="5"
-      mx="auto"
-      alignItems="center"
-      justifyContent="center"
-      maxW={{ base: "sm", md: "xl" }}
-      rounded="2xl"
-      borderWidth="1px"
-      borderColor="gray.300"
-      >
-        <Button
-          py="7"
-          fontSize="2xl"
-          colorScheme="twitter"
-          rounded="xl"
-          isDisabled={true}
-        >
-          <Spinner />
-        </Button>
-      </Flex>
-    );
+    const {address, isConnected, isConnecting} = useAccount();
+    const toast = useToast();
+    const [isLoading, setIsLoading] = useState(true); // Loading state
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false); // Set loading to false after initial check
+        }, 1000);
+        if (isConnected) {
+            handleConnect()
+        } else {
+            handleDisconnect()
+        }
+        return () => clearTimeout(timer); // Cleanup the timeout on unmount
+    }, [isConnected, address]); // Watch both isConnected and address
+    const handleConnect = () => {
+        toast({
+            title: "Wallet Connected",
+            description: "You have successfully connected your wallet.",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+        });
+    };
 
-  return (
-    <>
-    {
-      isConnected ? (
-        <SwapAmountProvider>
-          <WrapUnwrapETH />
-        </SwapAmountProvider>
-      ) : (
-        <Flex
-        gap="5"
-        my="auto"
-        p="5"
-        mx="auto"
-        alignItems="center"
-        justifyContent="center"
-        maxW={{ base: "sm", md: "xl" }}
-        rounded="2xl"
-        borderWidth="1px"
-        borderColor="gray.300"
-        >
-          <w3m-connect-button />
-        </Flex>
-      )
-    }
-    </>
-  )
+    const handleDisconnect = () => {
+        if (!isLoading) {
+            toast({
+                title: "Wallet Disconnected",
+                description: "You have successfully disconnected your wallet.",
+                status: "info",
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+    if (isConnecting)
+        return (
+            <Flex
+                gap="5"
+                my="auto"
+                p="5"
+                mx="auto"
+                alignItems="center"
+                justifyContent="center"
+                maxW={{base: "sm", md: "xl"}}
+                rounded="2xl"
+                borderWidth="1px"
+                borderColor="gray.300"
+            >
+                <Button
+                    py="7"
+                    fontSize="2xl"
+                    colorScheme="twitter"
+                    rounded="xl"
+                    isDisabled={true}
+                >
+                    <Spinner/>
+                </Button>
+            </Flex>
+        );
+
+    return (
+        <>
+            <Box
+                textAlign="center"
+                py={10}
+                px={6}
+                maxW="md"
+                mx="auto"
+                mt={10}
+                borderRadius="lg"
+                boxShadow="xl"
+                padding="4"
+                transition="all 0.3s ease"
+                // _hover={{
+                //     boxShadow: '2xl',
+                //     transform: 'scale(1.05)',
+                // }}
+            >
+                <VStack spacing={6}>
+                    <Heading as="h1" size="2xl">
+                        Welcome to Case Study
+                    </Heading>
+                    <Text fontSize="lg" color="gray.500">
+                        {isConnected
+                            ? `Connected as ${address?.substring(0, 6)}...${address?.substring(
+                                address?.length - 4
+                            )}`
+                            : "Connect your wallet to get started."}
+                    </Text>
+
+                    {isConnected ? (
+                        <SwapAmountProvider>
+                            <WrapUnwrapETH/>
+                        </SwapAmountProvider>
+                    ) : (
+                        <Flex
+                            gap="5"
+                            my="auto"
+                            p="5"
+                            mx="auto"
+                            alignItems="center"
+                            justifyContent="center"
+                            maxW={{base: "sm", md: "xl"}}
+                            rounded="2xl"
+                            borderWidth="1px"
+                            borderColor="gray.300"
+                        >
+                            <w3m-connect-button/>
+                        </Flex>
+                    )}
+                </VStack>
+            </Box>
+        </>
+    )
 }
 
 export default App
--- src/components/WrapUnWrapETH.tsx
+++ src/components/WrapUnWrapETH.tsx
@@ -1,163 +1,202 @@
 import {
-  Button,
-  Flex,
-  Spinner,
-  useToast,
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    Button,
+    Flex,
+    Spinner,
+    useDisclosure,
+    useToast
 } from "@chakra-ui/react"
-import React, { useEffect, useState } from "react"
-import { BaseError, useAccount, useBalance, useWriteContract } from "wagmi"
+import React, {useEffect, useRef, useState} from "react"
+import {BaseError, useAccount, useBalance, useWriteContract, useBlockNumber} from "wagmi"
 import SwapInput from "./SwapInput";
 import Navbar from "./Navbar";
-import { parseAbi, parseEther } from "viem";
-import { useBlockNumber } from "wagmi";
-import { useSwapAmount } from "../context/SwapAmountContext";
-import { config } from "../wagmi";
-import { waitForTransactionReceipt, WriteContractErrorType } from "wagmi/actions";
+import {parseAbi, parseEther} from "viem";
+import {useSwapAmount} from "../context/SwapAmountContext";
+import {config} from "../wagmi";
+import {waitForTransactionReceipt, WriteContractErrorType} from "wagmi/actions";
 
 const WETH_CONTRACT_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
 const WrapUnwrapETH: React.FC = () => {
-  const toast = useToast();
-  const { address } = useAccount();
-  const { amount } = useSwapAmount();
-  const [currentFrom, setCurrentFrom] = useState<"eth" | "weth">("eth");
-  const { data: ethBalance, refetch: ethRefetch } = useBalance({ address });
-  const { data: wethBalance, refetch: wethRefetch } = useBalance({ address, token: WETH_CONTRACT_ADDRESS });
-  const { data: blockNumber } = useBlockNumber({ watch: true });
+    const toast = useToast();
+    const {address} = useAccount();
+    const {amount} = useSwapAmount();
+    const [currentFrom, setCurrentFrom] = useState<"eth" | "weth">("eth");
+    const {data: ethBalance, refetch: ethRefetch} = useBalance({address});
+    const {data: wethBalance, refetch: wethRefetch} = useBalance({address, token: WETH_CONTRACT_ADDRESS});
+    const {data: blockNumber} = useBlockNumber({watch: true});
 
-  const [isConfirming, setIsConfirming] = useState<boolean>(false);
-  const { isPending, writeContract, error } = useWriteContract()
+    const [isConfirming, setIsConfirming] = useState<boolean>(false);
+    const {error, isPending, writeContract} = useWriteContract()
 
-  useEffect(() => {
-    ethRefetch();
-    wethRefetch();
-  }, [blockNumber]);
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const cancelRef = useRef<HTMLLIElement | null>(null);
+    const handleConfirm = () => {
+        onClose();
+        executeSwap()
+    };
 
-  const handleTransactionSubmitted = async (txHash: string) => {
-    setIsConfirming(true);
-    const transactionReceipt = await waitForTransactionReceipt(config, {
-      hash: txHash as `0x${string}`,
-    });
+    useEffect(() => {
+        ethRefetch();
+        wethRefetch();
+    }, [blockNumber]);
 
-    setIsConfirming(false);
-    // at this point the tx was mined
+    const handleTransactionSubmitted = async (txHash: string) => {
+        setIsConfirming(true);
+        const transactionReceipt = await waitForTransactionReceipt(config, {
+            hash: txHash as `0x${string}`,
+        });
 
-    if (transactionReceipt.status === "success") {
-      // execute your logic here
-      toast({
-        title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
-        description: "Successfully swapped.",
-        status: "success",
-        position: "bottom-right", 
-      })
-    } else {
-      toast({
-        title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
-        description: (error as BaseError).shortMessage || error?.message,
-        status: "error",
-        position: "bottom-right", 
-      })
-    }
-  }
+        setIsConfirming(false);
+        // at this point the tx was mined
 
-  const handleTransactionError = async (txError: WriteContractErrorType) => {
-    toast({
-      title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
-      description: (txError as BaseError).shortMessage || txError?.message,
-      status: "error",
-      position: "bottom-right", 
-    })
-  }
-
-  const executeSwap = async () => {
-    if (currentFrom === "eth") {
-      writeContract(
-        {
-          address: WETH_CONTRACT_ADDRESS,
-          abi: parseAbi(["function deposit() public payable"]),
-          functionName: 'deposit',
-          value: parseEther(amount || '0')
-        }, 
-        {
-          onSuccess: handleTransactionSubmitted,
-          onError: handleTransactionError,
-        }    
-      )
-    } else {
-      writeContract(
-        {
-          address: WETH_CONTRACT_ADDRESS,
-          abi: parseAbi(["function withdraw(uint wad) public"]),
-          functionName: 'withdraw',
-          args: [parseEther(amount || '0')]
-        },
-        {
-          onSuccess: handleTransactionSubmitted,
-          onError: handleTransactionError,
+        if (transactionReceipt.status === "success") {
+            // execute your logic here
+            toast({
+                title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
+                description: "Successfully swapped.",
+                status: "success",
+                position: "bottom-right",
+            })
+        } else {
+            toast({
+                title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
+                description: (error as BaseError).shortMessage || error?.message,
+                status: "error",
+                position: "bottom-right",
+            })
         }
-      )
     }
-  };
 
-  return (
-    <Flex
-      direction="column"
-      gap="5"
-      my="auto"
-      p="5"
-      mx="auto"
-      maxW={{ base: "sm", md: "xl" }}
-      w="full"
-      rounded="2xl"
-      borderWidth="1px"
-      borderColor="gray.300"
-    >
-      <Navbar />
-      <Flex
-        direction={currentFrom === "eth" ? "column" : "column-reverse"}
-        gap="3"
-      >
-        <SwapInput
-          current={currentFrom}
-          type="eth"
-          max={ethBalance?.formatted}
-          tokenSymbol="ETH"
-          tokenBalance={ethBalance?.formatted || "0"}
-        />
+    const handleTransactionError = async (txError: WriteContractErrorType) => {
+        toast({
+            title: currentFrom === "eth" ? "Wrap ETH" : "Unwrap WETH",
+            description: (txError as BaseError).shortMessage || txError?.message,
+            status: "error",
+            position: "bottom-right",
+        })
+    }
 
-        <Button
-          onClick={() =>
-            currentFrom === "eth"
-              ? setCurrentFrom("weth")
-              : setCurrentFrom("eth")
-          }
-          maxW="5"
-          mx="auto"
-          className="wrap-btn"
+    const executeSwap = async () => {
+        if (currentFrom === "eth") {
+            writeContract(
+                {
+                    address: WETH_CONTRACT_ADDRESS,
+                    abi: parseAbi(["function deposit() public payable"]),
+                    functionName: 'deposit',
+                    value: parseEther(amount || '0')
+                },
+                {
+                    onSuccess: handleTransactionSubmitted,
+                    onError: handleTransactionError,
+                }
+            )
+        } else {
+            writeContract(
+                {
+                    address: WETH_CONTRACT_ADDRESS,
+                    abi: parseAbi(["function withdraw(uint wad) public"]),
+                    functionName: 'withdraw',
+                    args: [parseEther(amount || '0')]
+                },
+                {
+                    onSuccess: handleTransactionSubmitted,
+                    onError: handleTransactionError,
+                }
+            )
+        }
+    };
+
+    return (
+        <Flex
+            direction="column"
+            gap="5"
+            my="auto"
+            p="5"
+            mx="auto"
+            maxW={{base: "sm", md: "xl"}}
+            w="full"
+            rounded="2xl"
+            borderWidth="1px"
+            borderColor="gray.300"
         >
-          ↓
-        </Button>
+            <Navbar/>
+            <Flex
+                direction={currentFrom === "eth" ? "column" : "column-reverse"}
+                gap="3"
+            >
+                <SwapInput
+                    current={currentFrom}
+                    type="eth"
+                    max={ethBalance?.formatted}
+                    tokenSymbol="ETH"
+                    tokenBalance={ethBalance?.formatted || "0"}
+                />
 
-        <SwapInput
-          current={currentFrom}
-          type="weth"
-          max={wethBalance?.formatted}
-          tokenSymbol="WETH"
-          tokenBalance={wethBalance?.formatted || "0"}
-          />
-      </Flex>
-      <Button
-        onClick={()=> {executeSwap()}}
-        py="7"
-        fontSize="2xl"
-        colorScheme="twitter"
-        rounded="xl"
-        isDisabled={isConfirming || isPending}
-      >
-        {(isConfirming || isPending) ? <Spinner /> : "Confirm"}
-      </Button>
-    </Flex>
-  )
+                <Button
+                    onClick={() =>
+                        currentFrom === "eth"
+                            ? setCurrentFrom("weth")
+                            : setCurrentFrom("eth")
+                    }
+                    maxW="5"
+                    mx="auto"
+                    className="wrap-btn"
+                >
+                    ↓
+                </Button>
+
+                <SwapInput
+                    current={currentFrom}
+                    type="weth"
+                    max={wethBalance?.formatted}
+                    tokenSymbol="WETH"
+                    tokenBalance={wethBalance?.formatted || "0"}
+                />
+            </Flex>
+            <Button
+                onClick={onOpen}
+                py="7"
+                fontSize="2xl"
+                colorScheme="twitter"
+                rounded="xl"
+                isDisabled={isConfirming || isPending || amount == '0'}
+            >
+                {(isConfirming || isPending) ? <Spinner/> : "Confirm"}
+            </Button>
+            <AlertDialog
+                isOpen={isOpen}
+                onClose={onClose}
+                leastDestructiveRef={cancelRef}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Swap
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure?
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme="twitter" onClick={handleConfirm} ml={3}>
+                                Confirm
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </Flex>
+    )
 }
 
 export default WrapUnwrapETH
